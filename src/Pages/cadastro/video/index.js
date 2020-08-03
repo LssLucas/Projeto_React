e1044a1b50e7c551e7a2114a0a8404b7d6a4e4eb
LssLucas <53626965+LssLucas@users.noreply.github.com>
2020-08-03 08:57:06 -0300

    Cadastro de videos (ainda com bug)

--- src/Pages/cadastro/video/index.js
+++ src/Pages/cadastro/video/index.js
@@ -1,27 +1,39 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import useForm from '../../../hooks/useForms';
 import Button from '../../../components/Button';
 import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
   const { handlerChange, values } = useForm({
     titulo: 'Vídeo',
     url: 'https://www.youtube.com/watch?v=k7IPr-rhrPE',
     categoria: 'Conheça o canal da Alura!',
-
   });
+  useEffect(() => {
+    categoriasRepository
+      .getAll()
+      .then((categoriasFromServer) => {
+        setCategorias(categoriasFromServer);
+      });
+  }, []);
 
   return (
     <PageDefault>
       <h1>Cadastrar Video</h1>
       <form onSubmit={(event) => {
         event.preventDefault();
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === values.categoria;
+        });
+        console.log('categoriaEscolhida', categoriaEscolhida);
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
@@ -47,7 +59,7 @@ function CadastroVideo() {
         />
         <FormField
           label="Categoria do Vídeo"
-          name="cate"
+          name="categoria"
           value={values.categoria}
           onChange={handlerChange}
         />
